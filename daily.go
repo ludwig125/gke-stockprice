19f872dc28d19692825a815ca8c077e83360794e
ludwig125 <hayakawa.sept4@gmail.com>
2020-07-17 06:42:14 +0900

    status management for all task in daily

--- daily.go
+++ daily.go
@@ -6,12 +6,12 @@ import (
 	"log"
 	"time"
 
+	"github.com/ludwig125/gke-stockprice/retry"
 	"github.com/ludwig125/gke-stockprice/sheet"
 	"github.com/ludwig125/gke-stockprice/status"
 )
 
 type daily struct {
-	currentTime          time.Time
 	status               sheet.Sheet
 	dailyStockPrice      DailyStockPrice
 	calculateMovingAvg   CalculateMovingAvg
@@ -32,40 +32,69 @@ func (d daily) exec(ctx context.Context, codes []string) error {
 		return nil
 	}
 
-	if d.currentTime.IsZero() {
-		log.Println("currentTime is zero")
-		return fmt.Errorf("currentTime is zero: %#v", d.currentTime)
+	if now().IsZero() {
+		log.Println("now is zero")
+		return fmt.Errorf("now is zero: %#v", now())
 	}
 
 	// Status管理用の変数
 	st := status.Status{Sheet: d.status}
 
 	// 日足株価のスクレイピングとDBへの書き込み
+	// statusシートを見て本日分が未完了であれば実行する(ExecIfIncompleteThisDay関数の機能)
 	sp := d.dailyStockPrice
-	failedCodes, err := sp.saveStockPrice(ctx, codes, d.currentTime)
-	if err != nil {
-		return fmt.Errorf("failed to fetchStockPrice: %v", mergeErr(err, failedCodes))
+	var failedCodes FailedCodes
+	if err := st.ExecIfIncompleteThisDay("saveStockPrice", now(), func() error {
+		var e error
+		failedCodes, e = sp.saveStockPrice(ctx, codes, now())
+		return e
+	}); err != nil {
+		return fmt.Errorf("failed to saveStockPrice: %v", mergeErr(err, failedCodes))
 	}
 
-	// TODO: 全部スクレイピングできていなかったら再度試みる処理を入れたい
-	// - failedCodesを使って、もう一度（または数回）saveStockPriceを実行する
+	// 全部スクレイピングできていなかったら再度試みる
+	// - failedCodesを使って、数回saveStockPriceを実行する
 	// - それでも失敗したものをあらためてfailedCodesとする
+	if len(failedCodes) > 0 {
+		retryCnt := 0
+		if err := retry.WithContext(ctx, 3, 3*time.Second, func() error {
+			fcodes := failedCodesSlice(failedCodes) // failedCodesから銘柄のスライスを取得
+			if len(fcodes) == 0 {                   // failedCodesがなければ終了
+				return nil
+			}
+			retryCnt++
+			log.Printf("retry: %d. trying to fetch stockprice for failed codes: %v", retryCnt, fcodes)
+			var e error
+			failedCodes, e = sp.saveStockPrice(ctx, fcodes, now()) // ここで改めてfailedCodesが上書きされる
+			st.UpdateStatus(fmt.Sprintf("saveStockPrice_retry%d", retryCnt), now())
+			return e
+		}); err != nil {
+			// retry 時のエラーはログに出すだけにしておく
+			log.Printf("failed to saveStockPrice in retry: %v", err)
+		}
+	}
+
 	// TODO: 最初に取得した株価が全部格納されているか確認したい
 
-	targetCodes := filterCodes(codes, failedCodes)
+	// この後の処理のために、失敗した銘柄以外を抜き出す。全部失敗して一つも残らなかったらエラーで終了
+	targetCodes := filterSuccessCodes(codes, failedCodes)
 	if len(targetCodes) == 0 {
-		return fmt.Errorf("all codes failed in saveStockPrice: %v", mergeErr(err, failedCodes))
+		return fmt.Errorf("all codes failed in saveStockPrice: %v", mergeErr(nil, failedCodes))
 	}
 
 	// 移動平均線の作成とDBへの書き込み
+	// statusシートを見て本日分が未完了であれば実行する
 	m := d.calculateMovingAvg
-	if err := m.saveMovingAvgs(ctx, targetCodes); err != nil {
-		return fmt.Errorf("failed to calcMovingAvg: %v", mergeErr(err, failedCodes))
+	if err := st.ExecIfIncompleteThisDay("saveMovingAvgs", now(), func() error {
+		return m.saveMovingAvgs(ctx, targetCodes)
+	}); err != nil {
+		return fmt.Errorf("failed to saveMovingAvgs: %v", mergeErr(err, failedCodes))
 	}
 
 	// 株価の増減トレンドをSpreadSheetに記載
+	// statusシートを見て本日分が未完了であれば実行する
 	g := d.calculateGrowthTrend
-	if err := st.ExecIfIncompleteThisDay("calculateGrowthTrend", d.currentTime, func() error {
+	if err := st.ExecIfIncompleteThisDay("calculateGrowthTrend", now(), func() error {
 		return g.growthTrend(ctx, targetCodes)
 	}); err != nil {
 		return fmt.Errorf("failed to growthTrend: %v", mergeErr(err, failedCodes))
@@ -74,8 +103,17 @@ func (d daily) exec(ctx context.Context, codes []string) error {
 	return mergeErr(nil, failedCodes)
 }
 
+// 失敗した銘柄をのスライスを返す関数
+func failedCodesSlice(failedCodes FailedCodes) []string {
+	fcodes := make([]string, len(failedCodes))
+	for i, f := range failedCodes {
+		fcodes[i] = f.code
+	}
+	return fcodes
+}
+
 // 全銘柄から失敗した銘柄を除く関数
-func filterCodes(codes []string, failedCodes FailedCodes) []string {
+func filterSuccessCodes(codes []string, failedCodes FailedCodes) []string {
 	match := func(code string, failedCodes FailedCodes) bool {
 		for _, f := range failedCodes {
 			if code == f.code {
