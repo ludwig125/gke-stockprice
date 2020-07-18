3e78bf41ada4a81b2891e5b0ac685a039ec9faa0
ludwig125 <hayakawa.sept4@gmail.com>
2020-07-19 08:04:51 +0900

    check previous date is day off

--- dayoff.go
+++ dayoff.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"time"
 
@@ -9,6 +10,31 @@ import (
 	"github.com/ludwig125/gke-stockprice/sheet"
 )
 
+// DayOff has dayoff(true/false) and it's reason.
+type DayOff struct {
+	dayOff bool
+	reason string
+}
+
+func isDayOff(previousDate time.Time, s sheet.Sheet) DayOff {
+	// 前の日が休日かどうか
+	isHoli, err := isHoliday(s, previousDate)
+	if err != nil {
+		// sheetからデータが取れないだけであればエラー出して処理自体は続ける
+		log.Printf("failed to check isHoliday: %v", err)
+	}
+	if isHoli {
+		return DayOff{dayOff: true, reason: fmt.Sprintf("%v is holiday", previousDate)}
+	}
+	// 前の日が土日かどうか
+	if isSaturdayOrSunday(previousDate) {
+		log.Println("previous day is saturday or sunday. finish task")
+		return DayOff{dayOff: true, reason: fmt.Sprintf("%v is saturday or sunday", previousDate)}
+	}
+
+	return DayOff{}
+}
+
 // spreadsheetの'holiday' sheetを読み取って 与えられた日付が祝日かどうか判断する
 func isHoliday(s sheet.Sheet, d time.Time) (bool, error) {
 	// 'holiday' sheet を読み取り
@@ -16,16 +42,16 @@ func isHoliday(s sheet.Sheet, d time.Time) (bool, error) {
 	// 東京証券取引所の休日: https://www.jpx.co.jp/corporate/calendar/index.html
 	holidays, err := s.Read()
 	if err != nil {
-		return true, errors.Wrap(err, "failed to ReadSheet")
+		return false, fmt.Errorf("failed to ReadSheet: %v", err)
 	}
 	if holidays == nil || len(holidays) == 0 {
-		return true, errors.New("no data in holidays")
+		return false, errors.New("no data in holidays")
 	}
 
 	date := d.Format("2006/01/02") // "2019/10/31" のようなフォーマットにする
 	log.Println("requested date:", date)
 	for _, h := range holidays {
-		if h[0] == date {
+		if h[0] == date { //対象の日付が一覧にあったら休日判定
 			return true, nil
 		}
 	}
